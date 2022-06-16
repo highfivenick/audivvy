f8f1afc22281ea8ed53a526e51c8a6edeec51634
highfivenick <nickhawkins.dev@gmail.com>
2022-06-16 11:26:47 -0400

    v2

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://highfive_nick:nick@cluster0.tqbln.mongodb.net/projects?retryWrites=true&w=majority', 
+    'dbName': 'projects'
+};
---
+++ public/main.js
@@ -0,0 +1,85 @@
+const thumbUp = document.getElementsByClassName("fa-thumbs-up");
+const trash = document.getElementsByClassName("fa fa-trash");
+const thumbDown = document.getElementsByClassName("fa-thumbs-down")
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('folders', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+Array.from(thumbDown).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('foldersDown', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('folders', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+
+
+let picker = document.getElementById('picker');
+let listing = document.getElementById('listing');
+
+picker.addEventListener('change', e => {
+  console.log(`we're trying`)
+  let fileArr = []
+  for (let file of Array.from(e.target.files)) {
+    fileArr.push(file)
+    let item = document.createElement('li');
+    item.textContent = file.webkitRelativePath;
+    listing.appendChild(item);
+  };
+  console.log(fileArr)
+});
